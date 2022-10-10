84959ebc6fab5183e4cb417db5718c19d76bd8ec
Dima <45449261+kitdim@users.noreply.github.com>
2022-10-10 20:31:50 +0300

    Update internal_settings.php
    Добавил автозагрузку классов

--- Core/base/settings/internal_settings.php
+++ Core/base/settings/internal_settings.php
@@ -22,3 +22,12 @@ const USER_CSS_JS = [
     'styles' => [],
     'scripts' => []
 ];
+function autoloadMainClasses($class_name){
+    $class_name = str_replace('\\', '/', $class_name);
+
+    if (!@include_once  $class_name . '.php'){
+        throw new RouteException('Неверное имя файла для подключения - '.$class_name);
+    }
+}
+
+spl_autoload_register('autoloadMainClasses');
